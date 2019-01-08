e3e67ba0d07fe2ab9904776dba1fc1edf88a4b46
alberto56 <albert@mediatribe.net>
2019-01-08 11:06:07 -0500

    Please squash this commit in the pull request

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 set -e
 docker pull node
 docker pull dcycle/broken-link-checker:1
-docker build -t local-dcycle-broken-link-checker-image:1 .
+docker build -t local-dcycle-broken-link-checker-image .
 
 docker run -v $(pwd)/example:/app/code dcycle/broken-link-checker:1 --help
 docker run -v $(pwd)/example:/app/code local-dcycle-broken-link-checker-image --help
