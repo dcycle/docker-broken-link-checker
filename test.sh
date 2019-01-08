6262e292864fe067b99ac1f949d620aa5aed5eb4
Albert Albala <albert@mediatribe.net>
2019-01-08 11:13:21 -0500

    Use correct image in automated test, and remove unnecessary volume sharing from example (#1)

--- test.sh
+++ test.sh
@@ -3,7 +3,5 @@ docker pull node
 docker pull dcycle/broken-link-checker:1
 docker build -t local-dcycle-broken-link-checker-image .
 
-docker run -v $(pwd)/example:/app/code dcycle/broken-link-checker --help
-docker run -v $(pwd)/example:/app/code dcycle/broken-link-checker .
+docker run -v $(pwd)/example:/app/code dcycle/broken-link-checker:1 --help
 docker run -v $(pwd)/example:/app/code local-dcycle-broken-link-checker-image --help
-docker run -v $(pwd)/example:/app/code local-dcycle-broken-link-checker-image .
