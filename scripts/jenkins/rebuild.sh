c39c23f696a91554f26111204d57b9d1889c2ba8
alberto56 <albert@mediatribe.net>
2020-10-22 15:47:29 -0400

    Remove duplicate code

--- scripts/jenkins/rebuild.sh
+++ scripts/jenkins/rebuild.sh
@@ -22,8 +22,7 @@ PRIVATE_IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
 IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
 echo "Created VM at $IP"
 sleep 90
-echo "Created Droplet at $IP"
-sleep 90
+
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   root@"$IP" "mkdir -p docker-broken-link-checker-job"
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
