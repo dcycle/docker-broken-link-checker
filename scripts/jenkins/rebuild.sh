9d8b978312ad31577bb6eaf097b5e87356c67e6a
alberto56 <albert@mediatribe.net>
2020-10-22 15:46:30 -0400

    https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses

--- scripts/jenkins/rebuild.sh
+++ scripts/jenkins/rebuild.sh
@@ -16,8 +16,12 @@ if [ -z "$DOCKERHOSTUSER" ] || [ -z "$DOCKERHOST" ]; then
 fi
 
 # Create a droplet
-IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
+PRIVATE_IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
   "./digitalocean/scripts/new-droplet.sh docker-broken-link-checker")
+# https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
+IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
+echo "Created VM at $IP"
+sleep 90
 echo "Created Droplet at $IP"
 sleep 90
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
