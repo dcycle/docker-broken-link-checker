38c5fd528fae7b37f4bb0b6ff62e9f22682deb78
Albert Albala <albert@mediatribe.net>
2022-11-06 19:42:39 -0500

    Please squash this commit in the pull request

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -5,6 +5,10 @@
 
 set -e
 
+# Always test before rebuilding as the API can have changed, for example at
+# https://github.com/phpstan/phpstan-src/commit/3e1ce5d06e574c6da93b8fa3dcf8569f8f8fcff3#diff-c9426bbd63493f10ff3790c0f0d13f99R39
+./test.sh
+
 CREDENTIALS="$HOME/.dcycle-docker-credentials.sh"
 
 if [ ! -f "$CREDENTIALS" ]; then
@@ -16,21 +20,30 @@ else
   source "$CREDENTIALS";
 fi
 
+./test.sh
+
 PROJECT=broken-link-checker
 DATE=`date '+%Y-%m-%d-%H-%M-%S-%Z'`
-MAJORVERSION='2'
-VERSION='2.0'
+MAJORVERSION='3'
+VERSION='3.0'
 
-# Start by getting the latest version of the official node image
-docker pull node
-# Rebuild the entire thing
-docker build --no-cache -t dcycle/"$PROJECT":"$VERSION" .
-docker build -t dcycle/"$PROJECT":"$MAJORVERSION" .
-docker build -t dcycle/"$PROJECT":"$MAJORVERSION".$DATE .
-docker build -t dcycle/"$PROJECT":"$VERSION".$DATE .
+# See https://github.com/dcycle/prepare-docker-buildx, for M1 native images.
+git clone https://github.com/dcycle/prepare-docker-buildx.git
+cd prepare-docker-buildx
+export DOCKER_CLI_EXPERIMENTAL=enabled
+./scripts/run.sh
+cd ..
+
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx inspect --bootstrap
 docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
-docker push dcycle/"$PROJECT":"$VERSION"
-docker push dcycle/"$PROJECT":"$MAJORVERSION"
-docker push dcycle/"$PROJECT":"$VERSION"."$DATE"
-docker push dcycle/"$PROJECT":"$MAJORVERSION"."$DATE"
-# Not using the latest tag, use the major version tag instead.
+
+# Start by getting the latest version of the base image
+docker pull dcycle/drupal:9php8-fpm-alpine
+# Rebuild the entire thing
+docker buildx build -t dcycle/"$PROJECT":"$VERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$MAJORVERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/"$PROJECT":"$VERSION".$DATE --platform linux/amd64,linux/arm64/v8 --push .
+# No longer using the latest tag, use the major version tag instead.
--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 set -e
 docker pull node
-docker pull dcycle/broken-link-checker:2
 docker build -t local-dcycle-broken-link-checker-image .
 
-docker run -v $(pwd)/example:/app/code dcycle/broken-link-checker:2 --help
 docker run -v $(pwd)/example:/app/code local-dcycle-broken-link-checker-image --help
