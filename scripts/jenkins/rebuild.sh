ee5d82a21dde7adaafa5a998523e398013b4757f
SKN <skn@SKNs-MacBook-Pro.local>
2024-06-09 22:42:54 +0530

    update rebuild script

--- scripts/jenkins/rebuild.sh
+++ scripts/jenkins/rebuild.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 set -e
-source ~/.docker-host-ssh-credentials
+
+# The token must be set as an environment variable
+if [ -v "$DIGITALOCEAN_ACCESS_TOKEN" ]; then
+  >&2 echo "Please set the DIGITALOCEAN_ACCESS_TOKEN environment variable"
+  exit 1
+fi
+# The ssh key must be set as an environment variable
+if [ -v "$DIGITALOCEAN_SSH_KEY" ]; then
+  >&2 echo "Please set the DIGITALOCEAN_SSH_KEY environment variable"
+  exit 1
+fi
 
 # Create a droplet
 DROPLET_NAME=docker-broken-link-checker
-IP1=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
-  "./digitalocean/scripts/new-droplet.sh -aubuntu $DROPLET_NAME")
-# https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
-IP2=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$IP1" --after-context=10|tail -1|cut -b 44-)
-echo "Now determining which of the IPs $IP1 or $IP2 is the public IP"
-if [[ $IP1 == 10.* ]]; then
-  IP="$IP2";
-else
-  IP="$IP1";
-fi
-echo "Created Droplet at $IP"
-sleep 90
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@"$IP" "mkdir -p docker-broken-link-checker-job"
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.dcycle-docker-credentials.sh root@$IP:~/.dcycle-docker-credentials.sh
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r * root@"$IP":docker-broken-link-checker-job
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@"$IP" "cd docker-broken-link-checker-job && ./scripts/install-docker-and-rebuild.sh"
+doctl compute droplet create "$DROPLET_NAME" --size 4gb --image ubuntu-20-04-x64 --ssh-keys "$DIGITALOCEAN_SSH_KEY"
+
+sleep 45
+
+IP=$(doctl compute droplet get "$DROPLET_NAME" --format PublicIPv4 --no-header)
+echo "Created Droplet at $IP"
+
+sleep 45
+
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  root@"$IP" "mkdir -p docker-docker-broken-link-checker"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  ~/.dcycle-docker-credentials.sh \
+  root@$IP:~/.dcycle-docker-credentials.sh
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  -r * root@"$IP":docker-docker-broken-link-checker
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+  root@"$IP" "cd docker-docker-broken-link-checker && ./scripts/install-docker-and-rebuild.sh"
